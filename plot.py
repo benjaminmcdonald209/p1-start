9c404edbd26d4f90b24977d08562b2ab7209b3d1
Ben McDonald <Ben@Bens-Air.hitronhub.home>
2020-03-27 17:51:30 -0600

    Cleaned up spacing.

--- plot.py
+++ plot.py
@@ -7,14 +7,14 @@ filename = sys.argv[1]        # Stores ARG1 in filename, as in: $ python plot.py
 data = np.loadtxt(filename,skiprows=32,delimiter=',')   # Attempts to load filename into local variable data.
 print(data)
 
-#filename = "raw-data/Sp15_245L_sect-001_group-1_glass.raw"
+# filename = "raw-data/Sp15_245L_sect-001_group-1_glass.raw"
 
 ## Part 0
 # Figure out what arguments to add to the loadtxt function call
 # so that numbers are loaded into the local function 'data'.
 # Hint: look for arguments like 'skiprows' and 'delimiter'
 # Check by running:
-#   $ python plot.py raw-data/Sp15_245L_sect-001_group-1_glass.raw
+# $ python plot.py raw-data/Sp15_245L_sect-001_group-1_glass.raw
 # at the command line.
 
 
@@ -32,12 +32,11 @@ strain = - data[:,7]
 iDash = filename.rindex('-')
 mylabel = filename[iDash+1:-4]
 
-plt.plot(strain,stress, color='k', label = 'Glass Data')
+plt.plot(strain,stress, color='k', label = mylabel)
 plt.xlabel('Strain [Ext %]')
 plt.ylabel('Stress [MPa]')
 plt.grid(True)
-plt.legend(loc = 'best')
-plt.show()
+
 
 ## Part 2
 # Check to see if your code in part 1 will plot all of the files in raw-data/
@@ -50,11 +49,12 @@ plt.show()
 # sure it makes sense! Use the slope of this line to calculate and print
 # the Young's modulus (with units!)
 
-first, second = np.polyfit(stress, strain, 1)
-linex = np.linspace(min(stress), max(stress))
+first, second = np.polyfit(strain, stress, 1)
+linex = np.linspace(min(strain), max(strain))
 liney = np.polyval([first,second], linex)
 
-plt.plot(linex,liney, color = 'r', linestyle = '-')
+plt.plot(linex,liney, color = 'r', linestyle = '-', label = "Liniar Fit")
+plt.legend(loc = 'best')
 plt.savefig(filename+ '.pdf')
 plt.show()
 
